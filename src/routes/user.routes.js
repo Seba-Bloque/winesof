7bf26218a9d38b0f7b8686d8f99ea1ea52c20b5b
Seba - Bloque <sebastian.saavedra@bloque.com.uy>
2023-07-31 16:08:30 -0300

    Auth JWT para los users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,8 @@ import { authenticateJWT } from '../config/auth.js';
 import * as userCtrl from "../controller/user.controller.js";
 import { validateUserId } from "../middleware/index.middlewares.js";
 
-router.get('/users', userCtrl.obtenerUsers);
-router.get('/users/:id', validateUserId, userCtrl.obtenerUnUser);
-router.delete('/users/:id', validateUserId, userCtrl.deleteUser);
+router.get('/users', authenticateJWT, userCtrl.obtenerUsers);
+router.get('/users/:id', authenticateJWT, validateUserId, userCtrl.obtenerUnUser);
+router.delete('/users/:id', authenticateJWT, validateUserId, userCtrl.deleteUser);
 
 export default router;
