a73cac99fe3fada7a07dfbc25371b61ef2fa7c3c
Seba - Bloque <sebastian.saavedra@bloque.com.uy>
2023-07-31 17:02:57 -0300

    Cambios en primsa

---
+++ prisma/migrations/20230731195739_image/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "bodega" ALTER COLUMN "image" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "event" ALTER COLUMN "image" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "vino" ALTER COLUMN "image" DROP NOT NULL;
